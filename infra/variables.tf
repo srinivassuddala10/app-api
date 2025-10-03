b492448f59c816b947ce4f554d3806a2e7f3f3bd
srinivassuddala10 <srinivassuddala10@gmail.com>
2025-10-03 16:01:55 +0530

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,6 +1,4 @@
 variable "aws_region" {}
-variable "ec2_instance_type" { default = "t2.micro" }
-variable "db_username" {}
-variable "db_password" {}
-variable "s3_bucket_name" {}
-variable "ecr_repo_name" {}
+variable "vpc_id" {}
+variable "key_name" {}
+
